0b8fe5a26590759bc56f3ab5dd2a66c709950167
Modulus <thejohnskauge@gmail.com>
2017-10-15 00:37:30 +0200

    Added install_docker.sh file

---
+++ install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-key fingerprint 0EBFCD88
+
+apt-get install docker-ce docker-compose
+
